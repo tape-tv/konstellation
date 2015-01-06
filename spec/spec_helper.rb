725827daead8359b6e34b23d94905042fdae1c35
chdorner <christof@chdorner.com>
2015-01-06 18:06:25 +0100

    yacs: Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,29 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yacs'
+
+Dir[File.expand_path('../support/*.rb', __FILE__)].sort.each do |file_path|
+  require file_path
+end
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 10
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
