9cfeff145c7e20cc34bfabb8b5fbad0719930dfb
chdorner <christof@chdorner.com>
2015-01-06 18:07:20 +0100

    yacs: Implement loading of YAML files
    With support for passing in default values.

--- lib/yacs.rb
+++ lib/yacs.rb
@@ -1,4 +1,20 @@
 require 'yacs/version'
 
+require 'yaml'
+require 'active_support/core_ext/hash/indifferent_access'
+
 module Yacs
+  def self.yaml_path(path)
+    Pathname.new(path)
+  end
+
+  def self.load(path, &defaults)
+    if yaml_path(path).exist?
+      config = YAML.load(yaml_path(path).open('r:utf-8'))
+    else
+      config = defaults.call
+    end
+
+    config.with_indifferent_access
+  end
 end
--- spec/yacs_spec.rb
+++ spec/yacs_spec.rb
@@ -1,11 +1,62 @@
 require 'spec_helper'
 
 describe Yacs do
-  it 'has a version number' do
-    expect(Yacs::VERSION).not_to be nil
+  subject { Yacs }
+
+  describe '.yaml_path' do
+    let(:path) { '/tmp/config.yml' }
+
+    it 'returns a Pathname' do
+      expect(subject.yaml_path(path)).to be_instance_of(Pathname)
+    end
+
+    it 'initializes a new Pathname with the passed-in path' do
+      expect(Pathname).to receive(:new).with(path)
+
+      subject.yaml_path(path)
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.load' do
+    include_context :uses_tmp_dir
+
+    let(:path) { File.join(tmp_dir, 'config.yml') }
+
+    context 'when the passed-in file path exists' do
+      before do
+        File.write(path, YAML.dump({
+          foo: 'bar',
+          'bar' => 'baz',
+          baz: 42
+        }))
+      end
+
+      it 'returns a correct hash based on the YAML' do
+        expect(subject.load(path)).to eq({
+          'foo' => 'bar',
+          'bar' => 'baz',
+          'baz' => 42
+        })
+      end
+
+      it 'allows indifferent access' do
+        expect(subject.load(path)['foo']).to eq('bar')
+        expect(subject.load(path)[:foo]).to eq('bar')
+      end
+
+      it 'does not use the passed in defaults' do
+        expect(subject.load(path) do
+          { 'foo' => 'baz' }
+        end[:foo]).to eq('bar')
+      end
+    end
+
+    context 'when the passed-in file path is missing' do
+      it 'uses the default' do
+        expect(subject.load(path) do
+          { 'foo' => 'baz' }
+        end[:foo]).to eq('baz')
+      end
+    end
   end
 end
